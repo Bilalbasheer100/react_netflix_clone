1fbea93ea579d8c86241e1829de99fb289fb826d
Bilalbasheer100 <mohammedbilalbasheer@gmail.com>
2022-10-18 19:39:24 +0530

    netflix with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+import Footer from './Components/Footer/footer'
+import {actions,originals,comedy, horror,romance,documentaries,anime} from './urls'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals' isSmall/>
+      <RowPost url={actions} title='Action Movies' isSmall/>
+      <RowPost url={comedy} title='Comedy Movies' isSmall/>
+      <RowPost url={romance} title='Romance Movies' isSmall/>
+      <RowPost url={horror} title='Horror Movies' isSmall/>
+      <RowPost url={documentaries} title='Documentaries' isSmall/>
+      <RowPost url={anime} title='Animations' isSmall/>
+      <Footer/>
+      
+
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl ='https://api.themoviedb.org/3';
+export const API_KEY='6590984d6b2355cfe05b0eb244d6c4a6';
+export const imageUrl='https://image.tmdb.org/t/p/original'
